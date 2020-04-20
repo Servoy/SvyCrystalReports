6f17e42ca38f475c76c55583154d3b6ddce882d6
sean <sdevlin@servoy.com>
2020-04-20 16:21:58 -0400

    remove license text

--- SvyCrystalReports/src/com/servoy/plugins/crystalreports/ResultSetProvider.java
+++ SvyCrystalReports/src/com/servoy/plugins/crystalreports/ResultSetProvider.java
@@ -1,32 +1,3 @@
-/*
- * ============================================================================
- * GNU Lesser General Public License
- * ============================================================================
- *
- * Servoy - Smart Technology For Smart Clients.
- * Copyright � 1997-2016 Servoy BV http://www.servoy.com
- * 
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- * 
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- * 
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307, USA.
- * 
- * Servoy B.V.
- * De Brand 26
- * 3823 LJ Amersfoort
- * The Netherlands
- * http://www.servoy.com
- */
-
 package com.servoy.plugins.crystalreports;
 
 import java.io.InputStream;
@@ -54,8 +25,6 @@ import java.util.List;
 import java.util.Map;
 import com.servoy.j2db.util.Debug;
 
-//import org.apache.logging.log4j.*;
-
 import com.servoy.j2db.dataprocessing.IFoundSet;
 import com.servoy.j2db.dataprocessing.IRecord;
 import com.servoy.j2db.persistence.RepositoryException;
@@ -65,8 +34,6 @@ import com.servoy.j2db.solutionmodel.ISMDataSourceNode;
 import com.servoy.plugins.crystalreports.ResultSetMetadataProvider;
 
 public class ResultSetProvider implements ResultSet {
-
-//	final static Logger logger = LogManager.getLogger(ResultSetProvider.class);
 	
 	private int currentIndex = 0;
 	protected IFoundSet foundset;
